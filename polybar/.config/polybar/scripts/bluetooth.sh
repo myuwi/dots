f275ebba0d3c081a64d5d20ae5afcb0ea55e7f39
myuwi <mikke012@gmail.com>
2022-03-03 21:19:14 +0200

    Removed unused config files

--- polybar/.config/polybar/scripts/bluetooth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]; then
-    echo "Off"
-else
-    echo $(bluetoothctl paired-devices | cut -d' ' -f2 | xargs -I{} -n1 bash -c "bluetoothctl info {} | grep -s 'Connected: yes'" | wc -l)
-fi
--- polybar/.config/polybar/scripts/brightnessctl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-brightness_level=$(brightnessctl g)
-brightness_max=$(brightnessctl m)
-brightness_percent_rounded=$(((($brightness_level * 100 / $brightness_max) + 2) / 5 * 5))
-
-# brightness_percent=$((($brightness_level * 100 / $brightness_max)))
-
-echo "$brightness_percent_rounded%"
