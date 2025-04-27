0bdc0caa98dda461263833dc2d16822ca343c400
myuwi <myuwi@proton.me>
2025-04-27 20:55:31 +0300

    refactor(nvim): simplify bufferline config

--- .config/nvim/lua/core/plugins/bufferline.lua
+++ .config/nvim/lua/core/plugins/bufferline.lua
@@ -1,88 +1,3 @@
-local function highlights()
-  local p = require("rose-pine.palette")
-
-  return {
-    fill = {
-      fg = p.text,
-      bg = p.base,
-    },
-    background = {
-      fg = p.highlight_high,
-      bg = p.base,
-    },
-    buffer_visible = {
-      fg = p.highlight_high,
-      bg = p.base,
-    },
-    buffer_selected = {
-      fg = p.text,
-      bg = p.surface,
-      bold = true,
-    },
-    close_button = {
-      fg = p.highlight_high,
-      bg = p.base,
-    },
-    close_button_visible = {
-      fg = p.highlight_high,
-      bg = p.base,
-    },
-    close_button_selected = {
-      fg = p.text,
-      bg = p.surface,
-    },
-    modified = {
-      fg = p.gold,
-      bg = p.base,
-    },
-    modified_visible = {
-      fg = p.gold,
-      bg = p.base,
-    },
-    modified_selected = {
-      fg = p.gold,
-      bg = p.surface,
-    },
-    duplicate_selected = {
-      fg = p.text,
-      bg = p.surface,
-      italic = false,
-    },
-    duplicate_visible = {
-      fg = p.highlight_med,
-      bg = p.base,
-      italic = false,
-    },
-    duplicate = {
-      fg = p.highlight_med,
-      bg = p.base,
-      italic = false,
-    },
-    indicator_selected = {
-      fg = p.surface,
-      bg = p.surface,
-    },
-    pick_selected = {
-      fg = p.love,
-      bg = p.surface,
-      bold = true,
-      italic = true,
-    },
-    pick_visible = {
-      fg = p.love,
-      bg = p.base,
-      bold = true,
-      italic = true,
-    },
-    pick = {
-      fg = p.love,
-      bg = p.base,
-      bold = true,
-      italic = true,
-    },
-  }
-end
-
 return {
   "akinsho/bufferline.nvim",
   dependencies = {
@@ -100,13 +15,13 @@ return {
     { "<Leader>bd", function() Snacks.bufdelete() end, desc = "Delete current buffer" },
   },
   opts = function()
+    local p = require("rose-pine.palette")
+
     return {
-      highlights = highlights(),
       options = {
+        indicator = { style = "none" },
         show_close_icon = false,
-        indicator = {
-          style = "none",
-        },
+        persist_buffer_sort = false,
         separator_style = { "", "" },
         -- stylua: ignore start
         close_command = function(n) Snacks.bufdelete(n) end,
@@ -114,8 +29,31 @@ return {
         right_mouse_command = false,
         -- stylua: ignore end
         offsets = {
-          { filetype = "NvimTree" },
+          { filetype = "NvimTree", highlight = "Normal" },
         },
+      },
+      highlights = {
+        fill = { bg = p.base },
+
+        background = { fg = p.highlight_high, bg = p.base },
+        buffer_visible = { fg = p.highlight_high, bg = p.base },
+        buffer_selected = { fg = p.text, bg = p.surface, bold = true },
+
+        duplicate = { fg = p.highlight_med, bg = p.base, italic = false },
+        duplicate_visible = { fg = p.highlight_med, bg = p.base, italic = false },
+        duplicate_selected = { fg = p.text, bg = p.surface, italic = false },
+
+        close_button = { fg = p.highlight_high, bg = p.base },
+        close_button_visible = { fg = p.highlight_high, bg = p.base },
+        close_button_selected = { fg = p.text, bg = p.surface },
+
+        modified = { fg = p.gold, bg = p.base },
+        modified_visible = { fg = p.gold, bg = p.base },
+        modified_selected = { fg = p.gold, bg = p.surface },
+
+        pick = { fg = p.love, bg = p.base, bold = true, italic = true },
+        pick_visible = { fg = p.love, bg = p.base, bold = true, italic = true },
+        pick_selected = { fg = p.love, bg = p.surface, bold = true, italic = true },
       },
     }
   end,
