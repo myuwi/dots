08f0f9b46ff7fafc241b8f0ee57b2b6cae99db16
myuwi <myuwi@proton.me>
2025-04-30 14:25:33 +0300

    style(nvim): remove extra braces around plugin definitions

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -12,27 +12,25 @@ local function debounce(fn, ms)
 end
 
 return {
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      events = { "BufWritePost", "BufReadPost", "TextChanged", "TextChangedI" },
-      linters_by_ft = {
-        python = { "flake8" },
-      },
+  "mfussenegger/nvim-lint",
+  opts = {
+    events = { "BufWritePost", "BufReadPost", "TextChanged", "TextChangedI" },
+    linters_by_ft = {
+      python = { "flake8" },
     },
-    config = function(_, opts)
-      local lint = require("lint")
-      lint.linters_by_ft = opts.linters_by_ft
-
-      local function try_lint()
-        lint.try_lint()
-      end
-
-      vim.api.nvim_create_autocmd(opts.events, {
-        callback = debounce(try_lint, 300),
-        desc = "lint buffer using nvim-lint",
-        group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-      })
-    end,
   },
+  config = function(_, opts)
+    local lint = require("lint")
+    lint.linters_by_ft = opts.linters_by_ft
+
+    local function try_lint()
+      lint.try_lint()
+    end
+
+    vim.api.nvim_create_autocmd(opts.events, {
+      callback = debounce(try_lint, 300),
+      desc = "lint buffer using nvim-lint",
+      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+    })
+  end,
 }
--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,111 +1,109 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      { "williamboman/mason.nvim", opts = {} },
-      "williamboman/mason-lspconfig.nvim",
-      "saghen/blink.cmp",
-      "b0o/schemastore.nvim",
-      { "folke/neoconf.nvim", opts = {} },
-      {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", opts = {} },
+    "williamboman/mason-lspconfig.nvim",
+    "saghen/blink.cmp",
+    "b0o/schemastore.nvim",
+    { "folke/neoconf.nvim", opts = {} },
+    {
+      "folke/lazydev.nvim",
+      ft = "lua",
+      opts = {
+        library = {
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
     },
-    opts = {
-      servers = {
-        "biome",
-        "clojure_lsp",
-        { "denols", opts = { mason = false } },
-        "elixirls",
-        "emmet_ls",
-        "eslint",
-        "fennel_ls",
-        { "gleam", opts = { mason = false } },
-        "gopls",
-        "jsonls",
-        "lemminx",
-        "marksman",
-        "prismals",
-        "pyright",
-        { "rust_analyzer", opts = { mason = false } },
-        "lua_ls",
-        "svelte",
-        "tailwindcss",
-        "taplo",
-        "tinymist",
-        "ts_ls",
-        "yamlls",
-      },
-      ---@type vim.diagnostic.Opts
-      diagnostic = {
-        virtual_text = true,
-        update_in_insert = true,
-        underline = true,
-        severity_sort = true,
-        float = {
-          border = "single",
-        },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.HINT] = "",
-            [vim.diagnostic.severity.INFO] = "",
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      local mason_lspconfig = require("mason-lspconfig")
-      local mason_lspconfig_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-      local on_attach = require("plugins.lsp.attach")
-
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          local bufnr = args.buf
-          on_attach(client, bufnr)
-        end,
-      })
-
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-      vim.lsp.config("*", {
-        capabilities = capabilities,
-      })
-
-      local function setup(server_name)
-        return vim.lsp.enable(server_name)
-      end
-
-      local ensure_installed = {}
-
-      for _, server in ipairs(opts.servers) do
-        local server_name = type(server) == "table" and server[1] or server
-        local server_opts = type(server) == "table" and server.opts or {}
-        local use_mason = server_opts.mason ~= false and vim.tbl_contains(mason_lspconfig_servers, server_name)
-
-        if use_mason then
-          ensure_installed[#ensure_installed + 1] = server_name
-        else
-          setup(server_name)
-        end
-      end
-
-      mason_lspconfig.setup({
-        ensure_installed = ensure_installed,
-        automatic_installation = false,
-        handlers = { setup },
-      })
-
-      vim.diagnostic.config(opts.diagnostic)
-    end,
   },
+  opts = {
+    servers = {
+      "biome",
+      "clojure_lsp",
+      { "denols", opts = { mason = false } },
+      "elixirls",
+      "emmet_ls",
+      "eslint",
+      "fennel_ls",
+      { "gleam", opts = { mason = false } },
+      "gopls",
+      "jsonls",
+      "lemminx",
+      "marksman",
+      "prismals",
+      "pyright",
+      { "rust_analyzer", opts = { mason = false } },
+      "lua_ls",
+      "svelte",
+      "tailwindcss",
+      "taplo",
+      "tinymist",
+      "ts_ls",
+      "yamlls",
+    },
+    ---@type vim.diagnostic.Opts
+    diagnostic = {
+      virtual_text = true,
+      update_in_insert = true,
+      underline = true,
+      severity_sort = true,
+      float = {
+        border = "single",
+      },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.HINT] = "",
+          [vim.diagnostic.severity.INFO] = "",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_lspconfig_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
+    local on_attach = require("plugins.lsp.attach")
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local bufnr = args.buf
+        on_attach(client, bufnr)
+      end,
+    })
+
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
+
+    local function setup(server_name)
+      return vim.lsp.enable(server_name)
+    end
+
+    local ensure_installed = {}
+
+    for _, server in ipairs(opts.servers) do
+      local server_name = type(server) == "table" and server[1] or server
+      local server_opts = type(server) == "table" and server.opts or {}
+      local use_mason = server_opts.mason ~= false and vim.tbl_contains(mason_lspconfig_servers, server_name)
+
+      if use_mason then
+        ensure_installed[#ensure_installed + 1] = server_name
+      else
+        setup(server_name)
+      end
+    end
+
+    mason_lspconfig.setup({
+      ensure_installed = ensure_installed,
+      automatic_installation = false,
+      handlers = { setup },
+    })
+
+    vim.diagnostic.config(opts.diagnostic)
+  end,
 }
