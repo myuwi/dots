4c7b7a316ebfb7df35bc9fe221ebbc3b4d2c58f7
myuwi <myuwi@proton.me>
2025-05-03 11:01:31 +0300

    refactor: move xrandr scripts to a single 'disp' script

--- .screenlayout/default.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --primary --mode 1920x1080 --rate 144.00 --pos 0x0 \
-	--output DP-3 --mode 1920x1080 --rate 60.00 --pos -1920x200 \
-	--output HDMI-0 --mode 1920x1080 --rate 60.00 --pos 1920x200
--- home/.config/awesome/config/autostart.lua
+++ home/.config/awesome/config/autostart.lua
@@ -1,11 +1,9 @@
 local helpers = require("helpers")
 
-local home_dir = os.getenv("HOME")
-
 -- Commands to run on startup
 local start_cmds = {
   "/usr/lib/xfce-polkit/xfce-polkit",
-  home_dir .. "/.dots/.screenlayout/default.sh",
+  "disp home",
   "picom",
   "solaar -w hide",
   "fcitx5",
