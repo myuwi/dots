dca980d7502ac38ad2630464a35a5f409b87af59
myuwi <myuwi@proton.me>
2025-05-07 10:07:19 +0300

    chore(nvim): update deprecated gitsigns settings

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -14,7 +14,6 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     opts = {
       current_line_blame = true,
-      current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
       on_attach = function(bufnr)
         local function opts(desc)
           return { buffer = bufnr, desc = desc, noremap = true, silent = true }
@@ -22,11 +21,13 @@ return {
 
         local gitsigns = require("gitsigns")
 
-        vim.keymap.set("n", "<leader>gp", gitsigns.prev_hunk, opts("Previous Hunk"))
-        vim.keymap.set("n", "<leader>gn", gitsigns.next_hunk, opts("Next Hunk"))
+        -- stylua: ignore start
+        vim.keymap.set("n", "<leader>gp", function() gitsigns.nav_hunk("prev") end, opts("Previous Hunk"))
+        vim.keymap.set("n", "<leader>gn", function() gitsigns.nav_hunk("next") end, opts("Next Hunk"))
         vim.keymap.set("n", "<leader>gg", gitsigns.preview_hunk_inline, opts("Preview Hunk Inline"))
         vim.keymap.set("n", "<leader>gtb", gitsigns.toggle_current_line_blame, opts("Toggle Current Line Blame"))
-        vim.keymap.set("n", "<leader>gtd", gitsigns.toggle_deleted, opts("Toggle Deleted"))
+        vim.keymap.set("n", "<leader>gtd", gitsigns.preview_hunk_inline, opts("Toggle Deleted"))
+        -- stylua: ignore end
       end,
     },
   },
