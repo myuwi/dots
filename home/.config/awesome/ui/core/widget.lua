e4dd5093f9a24434b1ed0e4e4f0d4899f69eb252
myuwi <myuwi@proton.me>
2025-05-19 15:56:37 +0300

    feat(awesome): add `widget.new` to crate widgets that can intergate with signals

---
+++ home/.config/awesome/ui/core/widget.lua
@@ -0,0 +1,98 @@
+local gtable = require("gears.table")
+local wibox = require("wibox")
+
+local Signal = require("ui.core.signal")
+
+local Widget = {}
+
+---@return boolean
+local function is_widget(obj)
+  return type(obj) == "table" and (obj.widget or obj.layout or rawget(obj, "is_widget"))
+end
+
+local function wrap(w, name, fn)
+  local old_fn = w[name]
+  w[name] = function(_, ...)
+    old_fn(w, fn(w, ...))
+  end
+end
+
+local function set_children(self, new_children)
+  if not new_children then
+    new_children = {}
+  elseif is_widget(new_children) then
+    new_children = { new_children }
+  end
+
+  for _, value in ipairs(self.children) do
+    for _, value_new in ipairs(new_children) do
+      if value == value_new then
+        goto continue
+      end
+    end
+
+    value:emit_signal("destroy")
+
+    ::continue::
+  end
+
+  return new_children
+end
+
+function Widget:on_destroyed(callback)
+  self:connect_signal("destroy", function()
+    callback()
+  end)
+
+  return self
+end
+
+function Widget.new(args)
+  if rawget(args, "is_widget") then
+    return args
+  end
+
+  args.widget = args.widget or args.layout
+
+  local new_widget = wibox.widget.base.make_widget_declarative({
+    widget = args.widget,
+  })
+
+  args.widget, args.layout = nil, nil
+
+  gtable.crush(new_widget, Widget, true)
+
+  -- TODO: special handling for add, reset, etc.?
+  wrap(new_widget, "set_children", set_children)
+
+  local children = {}
+
+  for key, value in pairs(args) do
+    if Signal.is_signal(value) then
+      -- TODO: reactive children?
+      if type(key) == "string" then
+        local unsub = value:subscribe(function(v)
+          new_widget[key] = v
+        end)
+
+        new_widget:on_destroyed(unsub)
+      end
+    elseif is_widget(value) then
+      children[#children + 1] = Widget.new(value)
+    else
+      new_widget[key] = value
+    end
+  end
+
+  new_widget.children = children
+
+  new_widget:on_destroyed(function()
+    for _, value in ipairs(new_widget.children) do
+      value:emit_signal("destroy")
+    end
+  end)
+
+  return new_widget
+end
+
+return Widget
--- home/.config/awesome/ui/shell/bar/clock.lua
+++ home/.config/awesome/ui/shell/bar/clock.lua
@@ -5,7 +5,8 @@ local wibox = require("wibox")
 
 local hshape = require("helpers.shape")
 
-local effect = require("ui.core.signal.effect")
+local widget = require("ui.core.widget")
+
 local computed = require("ui.core.signal.computed")
 local observe = require("ui.core.signal.observe")
 
@@ -17,7 +18,7 @@ local clock = function(s)
     return calendar_visible.value and calendar_popup.screen == s and beautiful.bg_focus or nil
   end)
 
-  local clock_widget = wibox.widget({
+  local clock_widget = widget.new({
     {
       {
         {
@@ -30,6 +31,7 @@ local clock = function(s)
       right = dpi(6),
       widget = wibox.container.margin,
     },
+    bg = bg,
     buttons = {
       awful.button({}, 1, function()
         awesome.emit_signal("shell::calendar_popup::show")
@@ -38,10 +40,6 @@ local clock = function(s)
     shape = hshape.rounded_rect(4),
     widget = wibox.container.background,
   })
-
-  effect(function()
-    clock_widget.bg = bg.value
-  end)
 
   return clock_widget
 end
