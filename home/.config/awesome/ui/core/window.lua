77807edd7d902991f51341a580bc9895f71b29e1
myuwi <myuwi@proton.me>
2025-05-22 22:46:48 +0300

    feat(awesome): subscribe to signals only if window is visible

--- home/.config/awesome/ui/core/window.lua
+++ home/.config/awesome/ui/core/window.lua
@@ -3,18 +3,33 @@ local widget = require("ui.core.widget")
 ---@class Window
 local Window = {}
 
+local function handle_attach_detach(w)
+  if w.widget and w.widget.emit_signal then
+    if w.visible then
+      w.widget:emit_signal("attach")
+    else
+      w.widget:emit_signal("detach")
+    end
+  end
+end
+
 function Window.new(args)
-  local window_type = args.window
+  local window_constructor = args.window
   args.window = nil
 
   args.widget = widget.new(args.widget)
 
-  local new_window = window_type(args)
+  local window = window_constructor(args)
 
-  -- TODO: emit this automatically when the widget changes
-  args.widget:emit_signal("attach")
+  -- TODO: also emit attach/detach when the widget changes
 
-  return new_window
+  if window.widget and window.visible then
+    window.widget:emit_signal("attach")
+  end
+
+  window:connect_signal("property::visible", handle_attach_detach)
+
+  return window
 end
 
 return Window
