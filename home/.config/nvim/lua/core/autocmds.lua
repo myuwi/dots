e4b12d9470ee8f357e9eb2b570546adbba4abeb5
myuwi <myuwi@proton.me>
2026-02-22 16:05:17 +0200

    feat(nvim): close empty buffers when another buffer is opened

--- home/.config/nvim/lua/core/autocmds.lua
+++ home/.config/nvim/lua/core/autocmds.lua
@@ -9,13 +9,27 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "qf",
-    "help",
-    "man",
-  },
+  group = vim.api.nvim_create_augroup("unlist-ephemeral-buffers", { clear = true }),
+  pattern = { "qf", "help", "man" },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.api.nvim_buf_set_keymap(event.buf, "n", "q", "<cmd>close<cr>", { silent = true })
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("auto-close-empty-buffer", { clear = true }),
+  callback = function()
+    vim.defer_fn(function()
+      local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+      if #bufs <= 1 then
+        return
+      end
+      for _, buf in ipairs(bufs) do
+        if buf.name == "" and buf.changed == 0 and buf.linecount <= 1 then
+          pcall(vim.api.nvim_buf_delete, buf.bufnr, { force = true })
+        end
+      end
+    end, 0)
   end,
 })
