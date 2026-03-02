2bf6bb69a1332374c234b480ea0236a8a8b4823e
myuwi <myuwi@proton.me>
2026-03-02 21:13:00 +0200

    chore(nvim): update autotag opts

--- home/.config/nvim/lua/plugins/autopairs.lua
+++ home/.config/nvim/lua/plugins/autopairs.lua
@@ -1,4 +1,13 @@
 return {
   { "windwp/nvim-autopairs", opts = {} },
-  { "windwp/nvim-ts-autotag", opts = {} },
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    },
+  },
 }
