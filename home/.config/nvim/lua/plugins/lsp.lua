223bfb87d0995810f1bc93e2e7f65f551c5f7468
myuwi <myuwi@proton.me>
2026-03-11 20:04:46 +0200

    feat(nvim): highlight symbol on hover

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -4,21 +4,30 @@ local function on_attach(client, bufnr)
     return { buffer = bufnr, desc = desc, noremap = true, silent = true }
   end
 
-  -- stylua: ignore
-  do
-    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts("Show diagnostics"))
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "single" }) end, opts("Hover symbol"))
-    vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts("Add diagnostics to location list"))
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts("Go to type definition"))
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Find references"))
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Find implementations"))
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename symbol"))
-    vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts("Rename symbol"))
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
-    vim.keymap.set("n", "<leader>gR", "<cmd>LspRestart<CR>", opts("Restart LSP client"))
+  local function hover()
+    vim.lsp.buf.hover({ border = "single" })
+    vim.lsp.buf.document_highlight()
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      buffer = bufnr,
+      once = true,
+      callback = function()
+        vim.lsp.buf.clear_references()
+      end,
+    })
   end
+
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts("Show diagnostics"))
+  vim.keymap.set("n", "K", hover, opts("Hover symbol"))
+  vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts("Add diagnostics to location list"))
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts("Go to type definition"))
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts("Find references"))
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Find implementations"))
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts("Rename symbol"))
+  vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts("Rename symbol"))
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code action"))
+  vim.keymap.set("n", "<leader>gR", "<cmd>LspRestart<CR>", opts("Restart LSP client"))
 
   -- TODO: Refresh when it becomes available?
   if client.server_capabilities.codeLensProvider then
