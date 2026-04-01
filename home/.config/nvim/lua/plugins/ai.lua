090bbb031c01d07638f47e7cf2a68e1320ec895a
myuwi <myuwi@proton.me>
2026-04-01 21:59:56 +0300

    chore(nvim): update opencode keybinds

--- home/.config/nvim/lua/plugins/ai.lua
+++ home/.config/nvim/lua/plugins/ai.lua
@@ -2,12 +2,11 @@ return {
   "nickjvandyke/opencode.nvim",
   -- stylua: ignore
   keys = {
-    { "<leader>oa", mode = {"n", "x"}, function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" } },
+    { "<leader>a", mode = {"n", "x"}, function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" } },
   },
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      -- Your configuration, if any; goto definition on the type or field for details
       server = {
         start = function() end,
         stop = function() end,
