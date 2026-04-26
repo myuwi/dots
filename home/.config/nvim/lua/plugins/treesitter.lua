c5129134bb94fd1ab6e85a926aa06323e3483e41
myuwi <myuwi@proton.me>
2026-04-26 21:07:46 +0300

    feat(nvim): update treesitter config

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,14 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  branch = "master",
-  main = "nvim-treesitter.configs",
-  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
-  ---@module "nvim-treesitter.configs"
-  ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
-  opts = {
-    ensure_installed = { "lua", "json", "toml", "comment", "markdown", "markdown_inline" },
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@call.outer",
-          ["ic"] = "@call.inner",
-        },
-        include_surrounding_whitespace = true,
-      },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    branch = "main",
+  },
+  {
+    "mks-h/treesitter-autoinstall.nvim",
+    opts = {
+      highlight = true,
     },
   },
 }
