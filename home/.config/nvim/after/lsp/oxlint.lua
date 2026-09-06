4b7ee2131a37e4be3613ba6599d249b0c908852f
myuwi <myuwi@proton.me>
2026-09-06 19:48:59 +0300

    feat(nvim): resolve oxlint root from oxlintrc

--- home/.config/nvim/after/lsp/oxlint.lua
+++ home/.config/nvim/after/lsp/oxlint.lua
@@ -2,6 +2,17 @@ local util = require("util")
 
 ---@type vim.lsp.Config
 return {
+  root_dir = function(bufnr, on_dir)
+    local git = vim.fs.root(bufnr, { ".git" })
+    local configs = vim.fs.find(".oxlintrc.json", {
+      upward = true,
+      limit = math.huge,
+      path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr)),
+      stop = git and vim.fs.dirname(git) or nil,
+    })
+
+    on_dir(configs[#configs] and vim.fs.dirname(configs[#configs]) or git)
+  end,
   cmd = function(dispatchers, config)
     local root_dir = config.root_dir
     local oxlint_bin_path = util.from_node_modules("oxlint", root_dir) or "oxlint"
