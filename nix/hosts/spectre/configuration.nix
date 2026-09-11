4877879cc9b9da82502509c16ed86a610c3e227c
myuwi <myuwi@proton.me>
2026-09-11 22:56:29 +0300

    feat(nix): add missing fonts

--- nix/hosts/spectre/configuration.nix
+++ nix/hosts/spectre/configuration.nix
@@ -52,6 +52,17 @@
 
   fonts.packages = [
     pkgs.nerd-fonts.jetbrains-mono
+    pkgs.inter
+    pkgs.material-symbols
+    pkgs.noto-fonts
+    (pkgs.runCommandNoCC "murecho" { } ''
+      install -Dm444 ${
+        pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/google/fonts/5174b3333331c966c38f4355d50b03ca1c1df2f9/ofl/murecho/Murecho%5Bwght%5D.ttf";
+          hash = "sha256-Oic8LxHgFk+Cm8FcBonlh/400Uk/8WfVr/+P5xop5mc=";
+        }
+      } $out/share/fonts/truetype/Murecho.ttf
+    '')
   ];
   fonts.fontconfig.defaultFonts = {
     serif = [ ];
